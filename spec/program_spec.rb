829aac2093c1e9dafb63b36509ab27ad1e04d3e8
echo storrs <echoatday@gmail.com>
2024-07-30 13:40:49 -0400

    Added a few more tests.

--- spec/program_spec.rb
+++ spec/program_spec.rb
@@ -24,9 +24,26 @@ describe Converter do
         expect(answer).to eql "76B"
     end
 
-    it "when I input a base out of range" do
+    it "when I convert 66 to base 36, I get 1U" do
+        converter = Converter.new
+        answer = converter.base_number 66,36
+        expect(answer).to eql "1U"
+    end
+    it "when I convert 66 to base 2, I get 1000010" do
+        converter = Converter.new
+        answer = converter.base_number 66,2
+        expect(answer).to eql "1000010"
+    end
+
+    it "when I input base 37, out of range, I get error text." do
         converter = Converter.new
         answer = converter.base_number 13,37
+        expect(answer).to eql "This format only works for base 2 to 36 number systems."
+    end
+
+    it "when I input base 1, out of range, I get error text." do
+        converter = Converter.new
+        answer = converter.base_number 13,1
         expect(answer).to eql "This format only works for base 2 to 36 number systems."
     end
 end
